acf8302d77493f331139c759af6042851f878d31
Abhishek Kumar <abhishekk580@gmail.com>
2020-09-06 01:09:37 +0530

    Updates

---
+++ precache-manifest.6c030559498b19c9961a2d2d1dc453c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881fee5c55113eff4034b69b431ab3d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0925a705b7843fe3871c",
+    "url": "/static/css/main.56a3fbc6.chunk.css"
+  },
+  {
+    "revision": "32955d90324d2cb93667",
+    "url": "/static/js/2.043c12d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.043c12d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0925a705b7843fe3871c",
+    "url": "/static/js/main.41344d34.chunk.js"
+  },
+  {
+    "revision": "8ac6c919551651ff71ef",
+    "url": "/static/js/runtime-main.45c2194a.js"
+  },
+  {
+    "revision": "f8fb67e1c2f170dd07b7f5dafa7c10ec",
+    "url": "/static/media/abhishek.f8fb67e1.jpg"
+  }
+]);
